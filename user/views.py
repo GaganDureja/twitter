609f22bc35c5e2e63488f4a179f3e82898331226
GaganDureja <gagandureja675@gmail.com>
2024-03-15 16:44:15 +0530

    home redirect and login check view added

--- user/views.py
+++ user/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
+
+
+
+def check_login(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    return render(request,'home/index.html')
+
+
+def home(request):
+    if request.user.is_authenticated:        
+        return render(request,'home/home.html')
+    return redirect('index')
