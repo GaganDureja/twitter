d19df8c547828dd76856442b19cb8c5368902798
GaganDureja <gagandureja675@gmail.com>
2024-03-15 16:56:40 +0530

    theme design static files removed

--- user/views.py
+++ user/views.py
@@ -8,10 +8,10 @@ from django.shortcuts import render, redirect
 def check_login(request):
     if request.user.is_authenticated:
         return redirect('home')
-    return render(request,'home/index.html')
+    return render(request,'tweet/index.html')
 
 
 def home(request):
     if request.user.is_authenticated:        
-        return render(request,'home/home.html')
+        return render(request,'tweet/home.html')
     return redirect('index')
