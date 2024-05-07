d0a58ef784131ebe84b54cd23c3877cea06032d9
GaganDureja <gagandureja675@gmail.com>
2024-05-07 13:35:52 +0530

    user app urls and static file url added

--- twitter/urls.py
+++ twitter/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from user.views import *
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', check_login, name='index'),
-]
+    path('', home, name='home'),
+    path('user/', include('user.urls')),
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
