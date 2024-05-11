d1f654929ebf16a455b5d4cdb93ce54b9b906c92
GaganDureja <gagandureja675@gmail.com>
2024-05-11 17:15:09 +0530

    create tweet function added

--- tweet/views.py
+++ tweet/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Tweet
+from django.contrib import messages
+from user.views import home
+
 
 # Create your views here.
+def add_tweet(request):
+    tweet_message = request.POST.get('tweet_message')
+    Tweet.objects.create(user = request.user, tweet_message = tweet_message)
+    messages.success(request, "Tweet uploaded")
+    return redirect('home')
