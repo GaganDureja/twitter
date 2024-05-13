9ba9d893d915fbd76634e0d8f0914d37f1729864
GaganDureja <gagandureja675@gmail.com>
2024-05-13 22:36:26 +0530

    signin and signup functions changed to separate function with get or post method. login and register username get from email split with @

--- user/views.py
+++ user/views.py
@@ -12,43 +12,50 @@ def home(request):
         return render(request,'home.html')
     return render(request,'user/login.html')
 
-def signup(request):
-    if request.user.is_authenticated:
-        return redirect('home')
-    if request.method == 'GET':
-        return render(request,'user/register.html')
-    email = request.POST.get('email')
-    first_name = request.POST.get('first_name')
-    last_name = request.POST.get('last_name')
-    password = make_password(request.POST.get('password'))
-    try:
-        User.objects.create(username=email, password=password, first_name=first_name, last_name=last_name, email=email)
-        messages.success(request, "Login to continue")
-    except Exception as e:
-        messages.error(request, e)
+def newRegistration(request):
+  if request.user.is_authenticated:
     return redirect('home')
+  return render(request,'user/register.html')
 
-def signin(request):
-    if request.user.is_authenticated:
-        return redirect('home')
-    if request.method == 'GET':
-        return render(request,'user/login.html')
-
-    email = request.POST.get('email')
-    password = request.POST.get('password')
-    select_user = User.objects.filter(email=email)
-
-    if not select_user:
-        messages.warning(request, "Account not found")
-        return redirect('home')
-
-    user = authenticate(request,username=email,password=password)
-    if not user:
-        messages.error(request, "Incorrect password!!!")
-        return redirect('home')
-
-    login(request, user)
+def createRegistration(request):
+  if request.user.is_authenticated or request.method != 'POST':
     return redirect('home')
+  email = request.POST.get('email')
+  username = email.split("@")[0]
+  first_name = request.POST.get('first_name')
+  last_name = request.POST.get('last_name')
+  password = make_password(request.POST.get('password'))
+  try:
+    User.objects.create(username=username, password=password, first_name=first_name, last_name=last_name, email=email)
+    messages.success(request, "Login to continue")
+  except Exception as e:
+    messages.error(request, e)
+  return redirect('home')
+
+def newSession(request):
+  if request.user.is_authenticated:
+    return redirect('home')
+  return render(request,'user/login.html')
+
+def createSession(request):
+  if request.user.is_authenticated or request.method != 'POST':
+    return redirect('home')
+  email = request.POST.get('email')
+  username = email.split("@")[0]
+  password = request.POST.get('password')
+  select_user = User.objects.filter(username=username)
+
+  if not select_user:
+      messages.warning(request, "Account not found")
+      return redirect('home')
+
+  user = authenticate(request,username=username,password=password)
+  if not user:
+      messages.error(request, "Incorrect password!!!")
+      return redirect('home')
+
+  login(request, user)
+  return redirect('home')
 
 def user_logout(request):
     logout(request)
