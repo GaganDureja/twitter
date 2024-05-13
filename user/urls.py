c507a3ee93c2a808b02d35c505cb1d0fde74ef3f
GaganDureja <gagandureja675@gmail.com>
2024-05-13 22:38:34 +0530

    signin and signup urls changed as for new views with seperate get and post method

--- user/urls.py
+++ user/urls.py
@@ -5,7 +5,9 @@ from . import views
 app_name = 'user'
 
 urlpatterns = [
-    path('signup', views.signup, name='Signup'),
-    path('signin', views.signin, name='Signin'),
-    path('logout', views.user_logout, name='Logout'),
+  path('registrations/new', views.newRegistration, name='newRegistration'),
+  path('registrations/create', views.createRegistration, name='createRegistration'),
+  path('sessions/new', views.newSession, name='newSession'),
+  path('sessions/create', views.createSession, name='createSession'),
+  path('logout', views.user_logout, name='Logout'),
 ]
