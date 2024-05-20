7dc94b718b0d1816bd793d73a79ecbc66a87886c
GaganDureja <gagandureja675@gmail.com>
2024-05-20 22:47:40 +0530

    edit tweet path,view and html page added

--- tweets/urls.py
+++ tweets/urls.py
@@ -6,4 +6,6 @@ app_name = 'tweets'
 urlpatterns = [
   path('create', views.createTweet, name='createTweet'),
   path('show/<int:id>', views.showTweet, name='showTweet'),
+  path('edit/<int:id>', views.editTweet, name='editTweet'),
+  path('update/<int:id>', views.updateTweet, name='updateTweet'),
 ]
--- tweets/views.py
+++ tweets/views.py
@@ -13,3 +13,20 @@ def createTweet(request):
 def showTweet(request,id):
   tweet_details = get_object_or_404(Tweet, id=id)
   return render(request, 'tweets/show.html',{'tweet_details':tweet_details})
+
+def editTweet(request,id):
+  tweet_details = get_object_or_404(Tweet, id=id)
+  if request.user != tweet_details.user:
+    messages.error(request, "Invalid path")
+    return redirect('home')
+  return render(request, 'tweets/edit.html',{'tweet_details':tweet_details})
+
+def updateTweet(request,id):
+  tweet_details = get_object_or_404(Tweet, id=id)
+  if request.user != tweet_details.user:
+    messages.error(request, "Invalid path")
+    return redirect('home')
+  tweet_details.tweet_message = request.POST.get('tweet_message')
+  tweet_details.save()
+  messages.success(request, "Tweet updated")
+  return redirect('home')
