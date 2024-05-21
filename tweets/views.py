13292be3f20b6fb04c2b65bcb8622af56524bd5f
GaganDureja <gagandureja675@gmail.com>
2024-05-21 14:03:11 +0530

    shorten code to check the request user and tweet user is same

--- tweets/views.py
+++ tweets/views.py
@@ -15,27 +15,18 @@ def showTweet(request,id):
   return render(request, 'tweets/show.html',{'tweet_details':tweet_details})
 
 def editTweet(request,id):
-  tweet_details = get_object_or_404(Tweet, id=id)
-  if request.user != tweet_details.user:
-    messages.error(request, "Invalid path")
-    return redirect('home')
+  tweet_details = get_object_or_404(Tweet, id=id, user_id=request.user.id)
   return render(request, 'tweets/edit.html',{'tweet_details':tweet_details})
 
 def updateTweet(request,id):
-  tweet_details = get_object_or_404(Tweet, id=id)
-  if request.user != tweet_details.user:
-    messages.error(request, "Invalid path")
-    return redirect('home')
+  tweet_details = get_object_or_404(Tweet, id=id, user_id=request.user.id)
   tweet_details.tweet_message = request.POST.get('tweet_message')
   tweet_details.save()
   messages.success(request, "Tweet updated")
   return redirect('home')
 
 def destroyTweet(request,id):
-  tweet_details = get_object_or_404(Tweet, id=id)
-  if request.user != tweet_details.user:
-    messages.error(request, "Invalid path")
-    return redirect('home')
+  tweet_details = get_object_or_404(Tweet, id=id, user_id=request.user.id)
   tweet_details.delete()
   messages.success(request, "Tweet deleted")
   return redirect('home')
