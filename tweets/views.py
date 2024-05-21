1d4e4d0a1eb7adf824d4f0b7aa6891314e77234b
GaganDureja <gagandureja675@gmail.com>
2024-05-21 22:23:22 +0530

    tweet_details varibale name changed to tweet

--- tweets/views.py
+++ tweets/views.py
@@ -11,22 +11,22 @@ def createTweet(request):
   return redirect('home')
 
 def showTweet(request,id):
-  tweet_details = get_object_or_404(Tweet, id=id)
-  return render(request, 'tweets/show.html',{'tweet_details':tweet_details})
+  tweet = get_object_or_404(Tweet, id=id)
+  return render(request, 'tweets/show.html',{'tweet':tweet})
 
 def editTweet(request,id):
-  tweet_details = get_object_or_404(Tweet, id=id, user_id=request.user.id)
-  return render(request, 'tweets/edit.html',{'tweet_details':tweet_details})
+  tweet = get_object_or_404(Tweet, id=id, user_id=request.user.id)
+  return render(request, 'tweets/edit.html',{'tweet':tweet})
 
 def updateTweet(request,id):
-  tweet_details = get_object_or_404(Tweet, id=id, user_id=request.user.id)
-  tweet_details.tweet_message = request.POST.get('tweet_message')
-  tweet_details.save()
+  tweet = get_object_or_404(Tweet, id=id, user_id=request.user.id)
+  tweet.tweet_message = request.POST.get('tweet_message')
+  tweet.save()
   messages.success(request, "Tweet updated")
   return redirect('home')
 
 def destroyTweet(request,id):
-  tweet_details = get_object_or_404(Tweet, id=id, user_id=request.user.id)
-  tweet_details.delete()
+  tweet = get_object_or_404(Tweet, id=id, user_id=request.user.id)
+  tweet.delete()
   messages.success(request, "Tweet deleted")
   return redirect('home')
