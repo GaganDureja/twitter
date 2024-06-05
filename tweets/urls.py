fc697643cf4f52e361eb230a0b0723a1e068dddf
GaganDureja <gagandureja675@gmail.com>
2024-06-05 17:34:15 +0530

    urls updated for class base view

--- tweets/urls.py
+++ tweets/urls.py
@@ -4,10 +4,10 @@ from . import views
 app_name = 'tweets'
 
 urlpatterns = [
-  path('create', views.createTweet, name='createTweet'),
-  path('show/<int:id>', views.showTweet, name='showTweet'),
-  path('edit/<int:id>', views.editTweet, name='editTweet'),
-  path('update/<int:id>', views.updateTweet, name='updateTweet'),
-  path('destroy/<int:id>', views.destroyTweet, name='destroyTweet'),
-  path('retweet/<int:id>', views.reTweet, name='reTweet')
+  path('create', views.TweetView.as_view(http_method_names=['post']), name='createTweet'),
+  path('show/<int:id>', views.TweetView.as_view(http_method_names=['get']), name='showTweet'),
+  path('edit/<int:id>', views.TweetEditView.as_view(http_method_names=['get']), name='editTweet'),
+  path('update/<int:id>', views.TweetEditView.as_view(http_method_names=['post']), name='updateTweet'),
+  path('destroy/<int:id>', views.TweetDestroyView.as_view(http_method_names=['get']), name='destroyTweet'),
+  path('retweet/<int:id>', views.RetweetView.as_view(http_method_names=['get']), name='reTweet')
 ]
