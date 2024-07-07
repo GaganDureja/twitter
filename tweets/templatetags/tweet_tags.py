9ca76101f94add52c112d6e371adbecdf47a3fee
Gagan Dureja <gagandureja675@gmail.com>
2024-07-07 19:45:34 +0530

    check original_tweet query more specific to id

--- tweets/templatetags/tweet_tags.py
+++ tweets/templatetags/tweet_tags.py
@@ -4,7 +4,7 @@ register = template.Library()
 
 @register.filter
 def can_edit_tweet(user, tweet):
-  return user.id == tweet.user_id and tweet.original_tweet is None
+  return user.id == tweet.user_id and tweet.original_tweet_id is None
 
 @register.filter
 def can_delete_tweet(user, tweet):
@@ -12,4 +12,4 @@ def can_delete_tweet(user, tweet):
 
 @register.filter
 def can_retweet(user, tweet):
-  return user.id != tweet.user_id and user.is_authenticated and tweet.original_tweet is None
+  return user.id != tweet.user_id and user.is_authenticated and tweet.original_tweet_id is None
