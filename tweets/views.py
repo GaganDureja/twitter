73736fc840452f6d99f2f2d052a814d9298d79e6
Gagan Dureja <gagandureja675@gmail.com>
2024-07-07 19:43:31 +0530

    url path, views and html changed to get post default methods

--- tweets/views.py
+++ tweets/views.py
@@ -3,9 +3,28 @@ from .models import Tweet
 from django.contrib import messages
 from django.views import View
 from django.utils import timezone
+from django.core.paginator import Paginator,PageNotAnInteger,EmptyPage
+from django.http import JsonResponse
 
 # Create your views here.
 class TweetsViews(View):
+  def get(self,request):
+    all_tweets = Tweet.objects.all().order_by('-id')
+    page = request.GET.get('page', 1)
+    paginator = Paginator(all_tweets, 10)
+    try:
+      tweets = paginator.page(page)
+    except (PageNotAnInteger, EmptyPage):
+      tweets = paginator.page(1)
+    return render(request,'home.html', {'all_tweets': tweets})
+
+  def post(self, request):
+    tweet_message = request.POST.get('tweet_message')
+    Tweet.objects.create(user = request.user, tweet_message = tweet_message)
+    messages.success(request, "Tweet uploaded")
+    return redirect('home')
+
+class TweetDetailViews(View):
   def dispatch(self, request, *args, **kwargs):
     action = kwargs.pop('action', None)
     if action and hasattr(self, action):
@@ -13,13 +32,7 @@ class TweetsViews(View):
       return handler(request, *args, **kwargs)
     return super().dispatch(request, *args, **kwargs)
 
-  def create(self, request):
-    tweet_message = request.POST.get('tweet_message')
-    Tweet.objects.create(user = request.user, tweet_message = tweet_message)
-    messages.success(request, "Tweet uploaded")
-    return redirect('home')
-
-  def show(self, request, id):
+  def get(self, request, id):
     tweet = get_object_or_404(Tweet, id=id)
     return render(request, 'tweets/show.html',{'tweet':tweet})
 
@@ -29,7 +42,7 @@ class TweetsViews(View):
       return HttpResponse("Retweets can't be retweeted", status=422)
     return render(request, 'tweets/edit.html',{'tweet':tweet})
 
-  def update(self, request, id):
+  def post(self, request, id):
     tweet = get_object_or_404(Tweet, id=id, user_id=request.user.id)
     if tweet.original_tweet is not None:
       return HttpResponse("Retweets can't be retweeted", status=422)
@@ -37,10 +50,11 @@ class TweetsViews(View):
     tweet.last_edited_at = timezone.now()
     tweet.save()
     messages.success(request, "Tweet updated")
-    return redirect('tweets:showTweet', id=id)
+    return redirect('tweets:DetailTweet', id=id)
 
-  def destroy(self, request, id):
+  def delete(self, request, id):
     tweet = get_object_or_404(Tweet, id=id, user_id=request.user.id)
     tweet.delete()
     messages.success(request, "Tweet deleted")
-    return redirect('home')
+    return JsonResponse({'status': 'success'})
+    # return redirect('home')
