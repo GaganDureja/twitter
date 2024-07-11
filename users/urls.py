4f53df4f7e7572bd15bcc188d1c563aed2faee7b
Gagan Dureja <gagandureja675@gmail.com>
2024-07-11 16:27:33 +0530

    url added for followers views

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
+from .followers import views as followers_views
 
 app_name = 'users'
 
 urlpatterns = [
   path('<int:id>', views.UserViews.as_view(), name="UserDetails"),
+  path('<int:id>/followers', followers_views.FollowersViews.as_view(), name='ManageFollower'),
   path('registrations/new', views.newRegistration, name='newRegistration'),
   path('registrations/create', views.createRegistration, name='createRegistration'),
   path('sessions/new', views.newSession, name='newSession'),
