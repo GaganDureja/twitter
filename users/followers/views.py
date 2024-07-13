f3150d92e3cad379a162f86f5c984b9d7a7a265f
Gagan Dureja <gagandureja675@gmail.com>
2024-07-13 16:55:57 +0530

    html and views function added to follow and unfollow a user

--- users/followers/views.py
+++ users/followers/views.py
@@ -8,13 +8,25 @@ import json
 
 class FollowersViews(View):
   def post(self, request, id):
-    user = get_object_or_404(User, id=id)
+    follow_to_user = get_object_or_404(User, id=id)
     if request.user.id==id:
       messages.error(request,'Cannot follow yourself')
       return JsonResponse({'status': 422})
-    if request.user.id in id.user.followers:
+    if request.user.id in follow_to_user.followers.all():
       messages.error(request,'You are already following')
       return JsonResponse({'status': 422})
-    request.user.followers.add(id)
+    follow_to_user.followers.add(request.user.id)
     messages.success(request, "Following now")
-    return JsonResponse({'status': 'success'})
+    return JsonResponse({'status': 200})
+
+  def delete(self, request, id):
+    unfollow_to_user = get_object_or_404(User, id=id)
+    if request.user.id==id:
+      messages.error(request,'Cannot unfollow yourself')
+      return JsonResponse({'status': 422})
+    if not unfollow_to_user.followers.filter(id=request.user.id).exists():
+      messages.error(request,'You are not following')
+      return JsonResponse({'status': 422})
+    unfollow_to_user.followers.remove(request.user.id)
+    messages.success(request, "Unfollowing now")
+    return JsonResponse({'status': 200})
