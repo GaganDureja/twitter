bf748283d9aa6b75f253440f489487440a635f05
Gagan Dureja <gagandureja675@gmail.com>
2024-07-15 17:53:28 +0530

    follow and unfollow tags modified to shorter code

--- users/templatetags/follower_tags.py
+++ users/templatetags/follower_tags.py
@@ -3,17 +3,7 @@ from django import template
 register = template.Library()
 
 @register.filter
-def can_follow(user, follow_to):
-  if user.id == follow_to.id:
-    return False
-  if user in follow_to.followers.all():
-    return False
-  return True
-
-@register.filter
-def can_unfollow(user, follow_to):
-  if user.id == follow_to.id:
-    return False
-  if user not in follow_to.followers.all():
+def is_following(following_user, followed_user):
+  if following_user in followed_user.followers.all():
     return False
   return True
