c67fb2eb96b5c1739ff732e2483cbb1ccf94b352
Gagan Dureja <gagandureja675@gmail.com>
2024-07-15 18:13:41 +0530

    follow and unfollow tags modified to shorter code

--- users/templatetags/follower_tags.py
+++ users/templatetags/follower_tags.py
@@ -4,6 +4,4 @@ register = template.Library()
 
 @register.filter
 def is_following(following_user, followed_user):
-  if following_user in followed_user.followers.all():
-    return False
-  return True
+  return following_user in followed_user.followers.all()
